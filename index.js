56bd268b167c2999e077e7898102022181e26a64
yonle <fledyonle@gmail.com>
2021-08-13 09:08:30 +0700

    Feature: Join & Leave channel

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// Link ffmpeg-static if there's. This is used for eris.
-try {
-	let ffmpegPath = require("ffmpeg-static").split("/");
-	ffmpegPath = ffmpegPath.slice(0, ffmpegPath.length-1).join("/");
-	process.env["PATH"] = ffmpegPath + ":" + process.env["PATH"];
-} catch (error) {}
-
 const openradio = require("openradio");
 const eris = require("eris");
 const { Server } = require("http");
@@ -90,7 +83,7 @@ bot.on("messageCreate", async message => {
 	let radio = radios.get(message.guildID);
 	switch (message.content.split(" ")[0].slice(1)) {
 		case "new": 
-			if (radio) return message.reply("You already created your radio. To manage it, Type /manage. To destroy it, Type /destroy");
+			if (radio) return message.reply("You already created your radio. To manage it, Type \*manage. To destroy it, Type \*destroy");
 			radios.set(message.guildID, {
 				player: new openradio().on('error', (err) => message.reply(err.toString())),
 				queue: [],
@@ -111,7 +104,7 @@ bot.on("messageCreate", async message => {
 					let nextSong = radio.queue.shift();
 					radio.metadata.curSong = null;
 					if (!nextSong) return false;
-					bot.sendChatAction(message.guildID, 'typing');
+					bot.sendChannelTyping(message.channel.id);
 					if (nextSong.type === "raw") {
 						let stream = miniget(nextSong.url);
 						stream.on('response', () => {
@@ -125,17 +118,17 @@ bot.on("messageCreate", async message => {
 						});
 
 						stream.on('request', () => {
-							bot.sendChatAction(message.guildID, 'typing');
+							bot.sendChannelTyping(message.channel.id);
 						});
 
 						stream.on('error', (err) => message.reply(err.toString()));
 					} else {
 					
-						bot.sendChatAction(message.guildID, 'typing');
+						bot.sendChannelTyping(message.channel.id);
 						let stream = ytdl(nextSong.id || nextSong.videoDetails.videoId, { filter: "audioonly", quality: "highestaudio" });
 
 						stream.on('info', (info) => {
-							bot.sendChatAction(message.guildID, 'typing');
+							bot.sendChannelTyping(message.channel.id);
 							radio.metadata.curSong = info;
 							radio.player.play(stream).then(radio.play);
 							if (radio.metadata.autoplay) {
@@ -150,21 +143,18 @@ bot.on("messageCreate", async message => {
 				}
 			});
 			radios.get(message.guildID).player.on('data', data => radios.get(message.guildID).metadata.listener.forEach((res, id) => res.write(data, err => {
-					if (err) {
-						radios.get(message.guildID).metadata.listener.delete(id);
-						console.error(err);
-					}
+				if (err) radios.get(message.guildID).metadata.listener.delete(id);
 			})));
 			message.reply("✔️Radio Created");
 			break;
 		case "destroy": 
-			if (!radio) return message.reply("You didn't created radio yet. Did you mean /new ?");
+			if (!radio) return message.reply("You didn't created radio yet. Did you mean \*new ?");
 			radio.player.destroy();
 			radios.delete(message.guildID);
 			message.reply("✔️Radio destroyed.");
 			break;
 		case "manage": 
-			if (!radio) return message.reply("You didn't created radio yet. Did you mean /new ?");
+			if (!radio) return message.reply("You didn't created radio yet. Did you mean \*new ?");
 			(() => {
 				let text = "**Your radio status**";
 				text += `\nListener: \`${radio.metadata.listener.size}\``;
@@ -177,12 +167,12 @@ bot.on("messageCreate", async message => {
 				text += `\nAutoplay Enabled?: \`${radio.metadata.autoplay ? "Yes" : "No"}\``;
 				text += `\nTotal Queue: \`${radio.queue.length}\``;
 				text += `\nLive on: ${process.env.SERVER_HOST||"http://localhost:3000"}/${message.guildID}`;
-				text += `\n\nTo check song queue, Type /queue`;
+				text += `\n\nTo check song queue, Type \*queue`;
 				message.reply(text);
 			})();
 			break;
 		case "queue":
-			if (!radio) return message.reply("You didn't created radio yet. Did you mean /new ?");
+			if (!radio) return message.reply("You didn't created radio yet. Did you mean \*new ?");
 			if (!radio.queue.length) return message.reply("🏜️Nothing is in queue....");
 			let method = message.content.split(" ").slice(1)[0];
 			if (!method) return (() => {
@@ -197,13 +187,13 @@ bot.on("messageCreate", async message => {
 						text += `\n${songNum}. **__${song.title}__**\nhttps://youtu.be/${song.id}`;
 					}
 				});
-				text += "\n\n⚠️Some song is hidden due to a lot of request value. We'll improve this soon.\n\nYou may also manage these queue. For more information, Do `/queue help`";
+				text += "\n\n⚠️Some song is hidden due to a lot of request value. We'll improve this soon.\n\nYou may also manage these queue. For more information, Do `*queue help`";
 				message.reply(text);
 			})();
 			
 			if (method === "help") {
 				let text = "**Queue Managing**";
-				text += "\nUsage: `/queue [method] [argument]`";
+				text += "\nUsage: `*queue [method] [argument]`";
 				text += "\n\nAvailable Method:";
 				text += "\n  remove  - Remove a song in a queue";
 				text += "\n  move    - Move a song in a queue";
@@ -212,7 +202,7 @@ bot.on("messageCreate", async message => {
 				message.reply(text);
 			} else if (method === "remove") {
 				let args = message.content.split(" ").slice(2)[0];
-				if (!args) return message.reply("Usage: `/queue remove [Order number of song in /queue]`");
+				if (!args) return message.reply("Usage: `*queue remove [Order number of song in \*queue]`");
 				if (!radio.queue[Number(args)-1]) return message.reply("No song was found in Queue Order number " + args);
 				delete radio.queue[Number(args)-1];
 				// Re-create. Ignore the undefined ones
@@ -221,7 +211,7 @@ bot.on("messageCreate", async message => {
 			} else if (method === "move") {
 				let args = message.content.split(" ").slice(2)[0];
 				let to = message.content.split(" ").slice(3)[0];
-				if (!args || !to) return message.reply("Usage: `/queue move [Order number] [To Order number]`");
+				if (!args || !to) return message.reply("Usage: `*queue move [Order number] [To Order number]`");
 				if (!radio.queue[Number(args)-1] || !radio.queue[Number(to)-1]) return message.reply("Song not found or invalid value.");
 				let fromOrder = radio.queue[Number(args)-1];
 				let toOrder = radio.queue[Number(to)-1];
@@ -234,20 +224,24 @@ bot.on("messageCreate", async message => {
 			}
 			break;
 		case "play":
-			if (!radio) return message.reply("You didn't created radio yet. Did you mean /new ?");
+			if (!radio) return message.reply("You didn't created radio yet. Did you mean \*new ?");
 			let str = message.content.split(" ").slice(1).join(" ");
 			let audio = message.reply_to_message ? message.reply_to_message.audio||message.reply_to_message.voice||message.reply_to_message.document : null;
 			if (!str.length && !audio) return message.reply("Usage: `/play [Song name|URL|Reply to Audio/Voice Message]`");
 			if (str) message.reply(`Searching \`${str}\`...`);
-			bot.sendChatAction(message.guildID, 'typing');
+			bot.sendChannelTyping(message.channel.id);
 
 			if (message.member.voiceState.channelID) {
 				bot.joinVoiceChannel(message.member.voiceState.channelID).catch(console.error).then(c => {
+					try {
+						c.piper.converterCommand = require("ffmpeg-static");
+					} catch {}
+					c.on('error', console.error);
 					if (c.playing) return;
+					if (radio.metadata.listener.has(message.guildID)) return c.play(radio.metadata.listener.get(message.guildID), { voiceDataTimeout: -1 });
 					let ply = new PassThrough();
 					radio.metadata.listener.set(message.guildID, ply);
 					c.play(ply, { voiceDataTimeout: -1 });
-					c.on('error', console.error);
 				});
 			}
 			if (str.toLowerCase().includes("youtube.com/playlist?list=")) {
@@ -259,7 +253,7 @@ bot.on("messageCreate", async message => {
 						radio.queue.push(res.items);
 						radio.queue = radio.queue.flat(Infinity);
 						message.reply("Preparing to play...");
-						bot.sendChatAction(message.guildID, 'typing');
+						bot.sendChannelTyping(message.channel.id);
 						radio.play();
 					} else {
 						radio.queue.push(res.items);
@@ -275,11 +269,11 @@ bot.on("messageCreate", async message => {
 				if (!radio.queue.length && !radio.metadata.curSong) {
 					radio.queue.push(newQueue);
 					message.reply("Preparing to play...");
-					bot.sendChatAction(message.guildID, 'typing');
+					bot.sendChannelTyping(message.channel.id);
 					radio.play();
 				} else {
 					radio.queue.push(newQueue);
-					bot.sendChatAction(message.guildID, 'typing');
+					bot.sendChannelTyping(message.channel.id);
 					message.reply("✔️A stream URL has been added to queue.");
 				}
 			} else if (ytdl.validateURL(str)) {
@@ -289,7 +283,7 @@ bot.on("messageCreate", async message => {
 					if (!radio.queue.length && !radio.metadata.curSong) {
 						radio.queue.push(info);
 						message.reply("Preparing to play...");
-						bot.sendChatAction(message.guildID, 'typing');
+						bot.sendChannelTyping(message.channel.id);
 						radio.play();
 					} else {
 						radio.queue.push(info);
@@ -298,14 +292,14 @@ bot.on("messageCreate", async message => {
 				});
 			} else {
 				ytsr(str, { limit: 1 }).then(res => {
-					bot.sendChatAction(message.guildID, 'typing');
+					bot.sendChannelTyping(message.channel.id);
 					res.items = res.items.filter(video => video.type == "video");
 					if (!res.items.length) return message.reply("🙅No Result.");
 					if (!radio) return;
 					if (!radio.queue.length && !radio.metadata.curSong) {
 						radio.queue.push(res.items[0]);
 						message.reply("Preparing to play...");
-						bot.sendChatAction(message.guildID, 'typing');
+						bot.sendChannelTyping(message.channel.id);
 						radio.play();
 					} else {
 						radio.queue.push(res.items[0]);
@@ -317,33 +311,33 @@ bot.on("messageCreate", async message => {
 			}
 			break;
 		case "pause":
-			if (!radio) return message.reply("You didn't created radio yet. Did you mean /new ?");
-			if (!radio.player.stream) return message.reply("There's nothing playing. Glitched? Do /destroy");
+			if (!radio) return message.reply("You didn't created radio yet. Did you mean \*new ?");
+			if (!radio.player.stream) return message.reply("There's nothing playing. Glitched? Do \*destroy");
 			radio.player.pause();
 			message.reply("⏸️Paused");
 			break;
 		case "resume":
-			if (!radio) return message.reply("You didn't created radio yet. Did you mean /new ?");
-			if (!radio.player.stream) return message.reply("There's nothing playing. Glitched? Do /destroy");
+			if (!radio) return message.reply("You didn't created radio yet. Did you mean \*new ?");
+			if (!radio.player.stream) return message.reply("There's nothing playing. Glitched? Do \*destroy");
 			radio.player.resume();
 			message.reply("▶️Resumed");
 			break;
 		case "skip":
-			if (!radio) return message.reply("You didn't created radio yet. Did you mean /new ?");
-			if (!radio.player.stream) return message.reply("There's nothing playing. Glitched? Do /destroy");
+			if (!radio) return message.reply("You didn't created radio yet. Did you mean \*new ?");
+			if (!radio.player.stream) return message.reply("There's nothing playing. Glitched? Do \*destroy");
 			if (!radio.queue.length) return message.reply("There's nothing in queue!");
 			radio.play();
 			message.reply("⏩Skipping...");
 			break;
 		case "stop":
-			if (!radio) return message.reply("You didn't created radio yet. Did you mean /new ?");
-			if (!radio.player.stream) return message.reply("There's nothing playing. Glitched? Do /destroy");
+			if (!radio) return message.reply("You didn't created radio yet. Did you mean \*new ?");
+			if (!radio.player.stream) return message.reply("There's nothing playing. Glitched? Do \*destroy");
 			radio.player.stream.destroy();
 			radio.queue = [];
 			message.reply("⏹️Player Stopped");
 			break;
 		case "autoplay":
-			if (!radio) return message.reply("You didn't created radio yet. Did you mean /new ?");
+			if (!radio) return message.reply("You didn't created radio yet. Did you mean \*new ?");
 			let autoplay = radio.metadata.autoplay;
 			if (radio.curSong.type == 'raw') return message.reply('Sorry. You can\'t use autoplay right now.');
 			if (!autoplay) {
@@ -357,7 +351,7 @@ bot.on("messageCreate", async message => {
 			}
 			break;
 		case "loop":
-			if (!radio) return message.reply("You didn't created radio yet. Did you mean /new ?");
+			if (!radio) return message.reply("You didn't created radio yet. Did you mean \*new ?");
 			(() => {
 				let loopType = message.content.split(" ").slice(1)[0];
 				let availableLoopType = ["queue", "single", "none"];
@@ -367,24 +361,30 @@ bot.on("messageCreate", async message => {
 				message.reply(`✔️Loop Type has been set as \`${loopType.toLowerCase()}\``);
 			})();
 			break;
+		case "leave":
+			if (!radio && !bot.voiceConnections.has(message.guildID) && !radio.metadata.listener.has(message.guildID)) return message.reply("I'm not in a voice channel or radio is not created.");
+			if (bot.voiceConnections.get(message.guildID).channelID !== message.member.voiceState.channelID) return message.reply("You're in different voice channel. Because of that, I'm aborting my action now.");
+			await bot.leaveVoiceChannel(bot.voiceConnections.get(message.guild.id).channelID);
+			break;
 		default:
 			//if (!message.content.startsWith("/start") || !message.content.startsWith("/help")) return;
 			(() => {
 				let text = "**OpenradioBot v0.0 Alpha**";
-				text += "\n\n__Radio Managing__";
-				text += "\n/new      - Create new radio";
-				text += "\n/destroy  - Destroy current radio";
-				text += "\n/manage   - Manage your radio";
-				text += "\n\n__Player Managing__";
-				text += "\n/play     - Play a song";
-				text += "\n/pause    - Pause a player";
-				text += "\n/resume   - Resume a player";
-				text += "\n/skip     - Skip current song";
-				text += "\n/stop     - Stop player";
-				text += "\n/queue    - See & Manage queue list.";
-				text += "\n/autoplay - Auto play next song from youtube **Related Videos** query.";
-				text += "\n/loop     - Loop queue";
-			
+				text += "\n\n**__Radio Managing__**";
+				text += "\n\*new      - Create new radio";
+				text += "\n\*destroy  - Destroy current radio";
+				text += "\n\*manage   - Manage your radio";
+				text += "\n\n**__Player Managing__**";
+				text += "\n\*play     - Play a song";
+				text += "\n\*pause    - Pause a player";
+				text += "\n\*resume   - Resume a player";
+				text += "\n\*skip     - Skip current song";
+				text += "\n\*stop     - Stop player";
+				text += "\n\*queue    - See & Manage queue list.";
+				text += "\n\*autoplay - Auto play next song from youtube **Related Videos** query.";
+				text += "\n\*loop     - Loop queue";
+				text += "\n\*leave    - Leae voice channel";
+				
 				message.reply(text);
 			})();
 			break;
